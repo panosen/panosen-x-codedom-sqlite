0cf3127fd29f8bb00b310f332038a95ccff1b773
hc_zhang <hc_zhang@trip.com>
2023-01-09 10:13:55 +0800

    0.3.1 add getArgsAsStrings

--- src/main/java/com/panosen/codedom/sqlite/engine/GenerationResponse.java
+++ src/main/java/com/panosen/codedom/sqlite/engine/GenerationResponse.java
@@ -21,4 +21,19 @@ public class GenerationResponse {
     public void setArgs(Object[] args) {
         this.args = args;
     }
+
+    public String[] getArgsAsStrings() {
+        if (args == null) {
+            return null;
+        }
+
+        String[] items = new String[args.length];
+        for (int index = 0, length = args.length; index < length; index++) {
+            if (args[index] != null) {
+                items[index] = args[index].toString();
+            }
+        }
+
+        return items;
+    }
 }
